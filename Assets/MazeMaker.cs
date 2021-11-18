db329907994320416ca35ff1b7bab80ec9a4fb8c
dninemfive <11084010+dninemfive@users.noreply.github.com>
2021-11-18 13:54:36 -0800

    probably should've been committing, huh

---
+++ Assets/MazeMaker.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.dninemfive.cmpm121.p3 {
+    public class MazeMaker : MonoBehaviour
+    {
+        const int MAX_RECURSION_DEPTH = 2;
+        public static Dictionary<(int x, int y), MazeRoom> MazeRooms;
+        public GameObject mazeRoomPrefab;
+        // not the same as DoorDirections mostly because i forgot, but also because they don't need to be interchangeable and it's more readable this way
+        public enum Direction : byte
+        {
+            NORTH = 0,
+            EAST = 1,
+            SOUTH = 2,
+            WEST = 3
+        }
+        public static void GenerateNeighbors(MazeRoom room, int iterations = 0)
+        {
+            foreach (Direction d in room.DoorDirections)
+            {
+                GenerateRoom(CoordsFrom(room.position, d), iterations);
+            }
+        }
+        public static void GenerateRoom((int x, int y) pos, int iterations = 0)
+        {
+            if (iterations >= MAX_RECURSION_DEPTH) return;
+            MazeRoom newRoom = new MazeRoom(pos);
+            MazeRooms[pos] = newRoom;
+            GenerateNeighbors(newRoom, (++iterations));
+        }
+
+        public static MazeRoom[] NeighborsOf((int x, int y) pos) 
+        {
+            return new MazeRoom[] { RoomFrom(pos, Direction.NORTH),
+                                    RoomFrom(pos, Direction.EAST),
+                                    RoomFrom(pos, Direction.SOUTH),
+                                    RoomFrom(pos, Direction.WEST) };
+        }
+
+        public static MazeRoom RoomAt((int x, int y) pos)
+        {
+            if (MazeRooms.ContainsKey(pos)) return MazeRooms[pos];
+            return null;
+        }
+        public static MazeRoom RoomFrom((int x, int y) pos, Direction d)
+        {
+            return RoomAt(CoordsFrom(pos, d));
+        }
+        public static (int x, int y) CoordsFrom((int x, int y) pos, Direction d)
+        {
+            var (x, y) = pos;
+            switch (d)
+            {
+                case Direction.NORTH:
+                    return (x, y + 1);
+                case Direction.SOUTH:
+                    return (x, y - 1);
+                case Direction.EAST:
+                    return (x + 1, y);
+                case Direction.WEST:
+                    return (x - 1, y);
+                default: throw new ArgumentNullException();
+            }
+        }
+    }    
+}
---
+++ Assets/MazeRoom.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace com.dninemfive.cmpm121.p3
+{
+    public class MazeRoom : MonoBehaviour
+    {
+        [Flags]
+        public enum DoorLocations : Byte
+        {
+            NONE = 0,
+            NORTH = 1,
+            EAST = 2,
+            SOUTH = 4,
+            WEST = 8
+        }
+        public DoorLocations doors;
+        public (int x, int y) position;
+
+        public void ToggleDoor(DoorLocations door)
+        {
+            // xoring a flag toggles it
+            doors ^= door;
+        }
+        public IEnumerable<MazeMaker.Direction> DoorDirections
+        {
+            get
+            {
+                if ((doors & DoorLocations.NORTH) == DoorLocations.NORTH) yield return MazeMaker.Direction.NORTH;
+                if ((doors & DoorLocations.EAST) == DoorLocations.EAST) yield return MazeMaker.Direction.EAST;
+                if ((doors & DoorLocations.SOUTH) == DoorLocations.SOUTH) yield return MazeMaker.Direction.SOUTH;
+                if ((doors & DoorLocations.WEST) == DoorLocations.WEST) yield return MazeMaker.Direction.WEST;
+            }
+        }
+        public bool CanBeHallway => doors == (DoorLocations.NORTH | DoorLocations.SOUTH) || doors == (DoorLocations.EAST | DoorLocations.WEST);
+        private bool _isHallway = false;
+        public bool HasDoorFacing(MazeMaker.Direction d)
+        {
+            switch (d)
+            {
+                case MazeMaker.Direction.NORTH:
+                    return (doors & DoorLocations.NORTH) == DoorLocations.NORTH;
+                case MazeMaker.Direction.EAST:
+                    return (doors & DoorLocations.EAST) == DoorLocations.EAST;
+                case MazeMaker.Direction.SOUTH:
+                    return (doors & DoorLocations.SOUTH) == DoorLocations.SOUTH;
+                case MazeMaker.Direction.WEST:
+                    return (doors & DoorLocations.WEST) == DoorLocations.WEST;
+                default: return false;
+            }
+        }
+        public void GenerateDoors()
+        {
+            int numDoors = UnityEngine.Random.Range(1, 4);
+            for (int i = 0; i < numDoors; i++)
+            {
+                // just writing this because it's cursed tbh
+                // note that this will produce duplicates sometimes. this is fine by me, since i want to bias toward fewer doors anyway.
+                doors |= (DoorLocations)(int)Math.Pow(2, UnityEngine.Random.Range(0, 3));
+            }
+            // add required doors
+            MazeRoom temp;
+            if ((temp = MazeMaker.RoomFrom(position, MazeMaker.Direction.SOUTH)) != null && temp.HasDoorFacing(MazeMaker.Direction.NORTH)) doors |= DoorLocations.SOUTH;
+            if ((temp = MazeMaker.RoomFrom(position, MazeMaker.Direction.WEST)) != null && temp.HasDoorFacing(MazeMaker.Direction.EAST)) doors |= DoorLocations.WEST;
+            if ((temp = MazeMaker.RoomFrom(position, MazeMaker.Direction.NORTH)) != null && temp.HasDoorFacing(MazeMaker.Direction.SOUTH)) doors |= DoorLocations.NORTH;
+            if ((temp = MazeMaker.RoomFrom(position, MazeMaker.Direction.EAST)) != null && temp.HasDoorFacing(MazeMaker.Direction.WEST)) doors |= DoorLocations.EAST;
+        }
+        public bool Hallway
+        {
+            get => _isHallway;
+            set
+            {
+                if (value)
+                {
+                    if (!CanBeHallway)
+                    {
+                        Debug.LogError("Tried to make a hallway out of an invalid room!");
+                    }
+                    _isHallway = value;
+                }
+                else
+                {
+                    _isHallway = value;
+                }
+            }
+        }
+        public MazeRoom((int x, int y) pos)
+        {
+            position = pos;
+            GenerateDoors();
+        }
+    }
+}
