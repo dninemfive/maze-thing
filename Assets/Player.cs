ba7480c67423f81daa2e7d91a0ac137f7511ead0
dninemfive <11084010+dninemfive@users.noreply.github.com>
2021-11-22 09:18:38 -0800

    add player movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,6 +7,8 @@ namespace com.dninemfive.cmpm121.p3
     public class Player : MonoBehaviour
     {
         public static GameObject ActivePlayer;
+        public CharacterController controller;
+        public float speed = 10;
         // Start is called before the first frame update
         void Start()
         {
@@ -16,7 +18,18 @@ namespace com.dninemfive.cmpm121.p3
         // Update is called once per frame
         void Update()
         {
-
+            float v = Input.GetAxis("Vertical"),
+                  h = Input.GetAxis("Horizontal");
+            Vector3 velocity = Vector3.zero;
+            velocity += transform.forward * h * speed;
+            velocity += transform.right * v * speed;
+            controller.SimpleMove(velocity);
+            float rot = 0;
+            if (Input.GetKey(KeyCode.Q)) rot -= 1;
+            if (Input.GetKey(KeyCode.E)) rot += 1;
+            Vector3 angle = transform.eulerAngles;
+            angle.y += rot * Time.deltaTime * 60;
+            transform.eulerAngles = angle;
         }
     }
 }
